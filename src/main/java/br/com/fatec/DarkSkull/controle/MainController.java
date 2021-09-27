b3a06fa64dcac26d903103d1079efb1a53947baf
Bruno Abner <bruno.santos@kaffa.com.br>
2021-09-27 08:47:38 -0300

    mudança

--- src/main/java/br/com/fatec/DarkSkull/controle/MainController.java
+++ src/main/java/br/com/fatec/DarkSkull/controle/MainController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController extends DarkSkullController {
+public class MainController {
 
     @GetMapping(value = "/")
     public String index() {
